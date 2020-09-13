8199b957bdc56d53ab6c76775513853c7bdad53b
William G <wmgraves@vt.edu>
2020-09-13 11:53:56 -0400

    Implemented Kernel Tests
    - Created quick tests for verifying that kernels are working

--- src/ProgramUI/Driver.java
+++ src/ProgramUI/Driver.java
@@ -3,6 +3,7 @@ package ProgramUI;
 import IO.PatternPreviewCreator;
 import ImageProcessing.ColorPicker;
 import ImageProcessing.ColorTransform;
+import ImageProcessing.Kernel;
 import ImageProcessing.MedianCutQuantization;
 
 import javax.imageio.ImageIO;
@@ -43,18 +44,24 @@ public class Driver {
         settings.put("Output File Path", "C:\\Users\\William\\Desktop\\");
 
         // Process image based on user inputs
+        //TODO: Add UI/method for this
         ArrayList<ArrayList<Color>> colorGroups = MedianCutQuantization.quantize(image,
                 (Integer) settings.get("Number of Colors"));
         ArrayList<Color> averageColors = ColorPicker.averageColors(colorGroups);
 
-        BufferedImage resultImage = new BufferedImage(image.getWidth(), image.getHeight(),
-                BufferedImage.TYPE_INT_ARGB);
-        ColorTransform.transformColors(image, resultImage, averageColors);
+        int width = image.getWidth()/2, height = image.getHeight()/2;
+        BufferedImage patternPreview;// = ColorTransform.transformColors(image, averageColors);
+        BufferedImage kernelImage = Kernel.visualizeKernel(image, Kernel.KernelType.LANCZOS, width, height);
+        patternPreview = Kernel.applyKernel(image, Kernel.KernelType.LANCZOS, width, height);
+        patternPreview = ColorTransform.transformColors(patternPreview, averageColors);
 
         // Create a new image to show the result
+        //TODO: Add method/UI for this
         boolean success = true;
-        success = success && PatternPreviewCreator.createPNGFile(resultImage, settings.get("Output File Path")
+        success = success && PatternPreviewCreator.createPNGFile(patternPreview, settings.get("Output File Path")
                 + "preview.png");
+        success = success && PatternPreviewCreator.createPNGFile(kernelImage, settings.get("Output File Path")
+                + "kernel.png");
 
         if (success) {
             System.out.println("\nAll output files created successfully");
