4aacbd9294eca6deaf90165b5ab524b1e1a373e0
William G <wmgraves@vt.edu>
2020-09-13 17:23:45 -0400

    Created File Select UI
    - Began using JFileChooser for selecting an input file instead of using a hard-coded filepath
    - Changed how test images are generated

--- src/ProgramUI/Driver.java
+++ src/ProgramUI/Driver.java
@@ -7,6 +7,8 @@ import ImageProcessing.Kernel;
 import ImageProcessing.MedianCutQuantization;
 
 import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.IndexColorModel;
@@ -14,30 +16,44 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Handles running the entire program.
  */
 public class Driver {
     /**
-     * Main method
+     * Main method that runs the program
      * @param args unused
      */
     public static void main(String[] args) {
-        //TODO: Add UI/method for selecting file to use
-        String filePath = "C:\\Users\\William\\Desktop\\EeveeEvolutionPrismCropped.png";
+        // Get the input image file
+        System.out.println("Prompting user to select file...");
+        JFileChooser fileSelector = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Image Files", "jpg", "png");
+        fileSelector.setFileFilter(filter);
+        fileSelector.setMultiSelectionEnabled(false);
 
-        // Get a usable image from the selected file
+        int returnValue = fileSelector.showOpenDialog(null);
+        if (returnValue != JFileChooser.APPROVE_OPTION) {
+            System.out.println("No file was selected, aborting program");
+            return;
+        }
+        System.out.println("File \"" + fileSelector.getSelectedFile() + "\" selected");
+
         BufferedImage image;
         try {
-            image = ImageIO.read(new File(filePath));
+            image = ImageIO.read(fileSelector.getSelectedFile());
         } catch (IOException e) {
             e.printStackTrace();
             return;
         }
 
+        new SettingsUI(image);
+
         // Get info from user about desired pattern
         //TODO: Add UI/method for this
+        HashMap<String, BufferedImage> imagesToSave = new HashMap<>();
         HashMap<String, Object> settings = new HashMap<>();
         settings.put("Count", new Integer(18));
         settings.put("Number of Colors", new Integer(100));
@@ -47,27 +63,30 @@ public class Driver {
         //TODO: Add UI/method for this
         ArrayList<ArrayList<Color>> colorGroups = MedianCutQuantization.quantize(image,
                 (Integer) settings.get("Number of Colors"));
-        ArrayList<Color> averageColors = ColorPicker.averageColors(colorGroups);
+        ArrayList<Color> colorList = ColorPicker.averageColors(colorGroups);
 
-        int width = image.getWidth()/2, height = image.getHeight()/2;
-        BufferedImage patternPreview;// = ColorTransform.transformColors(image, averageColors);
-        BufferedImage kernelImage = Kernel.visualizeKernel(image, Kernel.KernelType.LANCZOS, width, height);
-        patternPreview = Kernel.applyKernel(image, Kernel.KernelType.LANCZOS, width, height);
-        patternPreview = ColorTransform.transformColors(patternPreview, averageColors);
+        int width = 324, height = 216;
+        BufferedImage currentImage = Kernel.applyKernel(image, Kernel.KernelType.LANCZOS, width, height);
+        imagesToSave.put("ScaledDownImage", currentImage);
+        currentImage = Kernel.applyKernel(currentImage, Kernel.KernelType.SHARPEN, width, height);
+        imagesToSave.put("SharpensImage", currentImage);
+        currentImage = ColorTransform.transformColors(currentImage, colorList);
+        imagesToSave.put("FinalImage", currentImage);
 
         // Create a new image to show the result
         //TODO: Add method/UI for this
+        System.out.println("\nSaving " + imagesToSave.size() + " output files...");
         boolean success = true;
-        success = success && PatternPreviewCreator.createPNGFile(patternPreview, settings.get("Output File Path")
-                + "preview.png");
-        success = success && PatternPreviewCreator.createPNGFile(kernelImage, settings.get("Output File Path")
-                + "kernel.png");
+        for (Map.Entry<String, BufferedImage> entry : imagesToSave.entrySet()) {
+            success = success && PatternPreviewCreator.createPNGFile(entry.getValue(),
+                    settings.get("Output File Path") + entry.getKey() + ".png");
+        }
 
         if (success) {
-            System.out.println("\nAll output files created successfully");
+            System.out.println("All output files created successfully");
         }
         else {
-            System.out.println("\nSome (or all) output files were not created successfully");
+            System.out.println("Some (or all) output files were not created successfully");
         }
     }
 
