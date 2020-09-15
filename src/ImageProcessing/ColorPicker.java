c6cc95225c223db913a062c126565ebdbe963a1f
William G <wmgraves@vt.edu>
2020-09-14 23:21:17 -0400

    Implemented Floss Color Matching
    - Created method for removing duplicate colors
    - Created method for matching colors to specific floss colors from the data sheet

--- src/ImageProcessing/ColorPicker.java
+++ src/ImageProcessing/ColorPicker.java
@@ -1,9 +1,10 @@
 package ImageProcessing;
 
+import IO.Triplet;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.*;
 
 /**
  * Handles calculating color information
@@ -15,7 +16,7 @@ public class ColorPicker {
      * @return The average color
      */
     public static ArrayList<Color> averageColors(ArrayList<ArrayList<Color>> colorGroups) {
-        System.out.println("\nAveraging color groups...");
+        System.out.println("Averaging color groups...");
         ArrayList<Color> averagedColors = new ArrayList<>();
 
         for (ArrayList<Color> colorGroup : colorGroups) {
@@ -33,5 +34,49 @@ public class ColorPicker {
 
         System.out.println("Finished averaging color groups");
         return averagedColors;
+    }
+
+    public static ArrayList<Color> removeDuplicates(ArrayList<Color> colors) {
+        ArrayList<Color> returnList = new ArrayList<>();
+
+        for (Color color : colors) {
+            if (!returnList.contains(color)) {
+                returnList.add(color);
+            }
+        }
+
+        return returnList;
+    }
+
+    public static ArrayList<Color> convertIntoFlossColors(HashMap<Color,
+            Triplet<String, String, Boolean>> colorList, ArrayList<Color> colors) {
+        ArrayList<Color> returnList = new ArrayList<>();
+
+        for (Color color : colors) {
+            returnList.add(getClosestFlossColor(colorList, color).getKey());
+        }
+
+        return returnList;
+    }
+
+    public static Map.Entry<Color, Triplet<String, String, Boolean>> getClosestFlossColor(HashMap<Color,
+            Triplet<String, String, Boolean>> colorList, Color color) {
+        if (colorList.containsKey(color)) { return new AbstractMap.SimpleEntry<>(color, colorList.get(color)); }
+
+        Color closestColor = null;
+        int minColorDistance = -1;
+        for (Color currentColor : colorList.keySet()) {
+            int temp = ColorTransform.getColorDistance(color, currentColor);
+            if (closestColor == null) {
+                closestColor = currentColor;
+                minColorDistance = temp;
+            }
+            else if (temp < minColorDistance) {
+                closestColor = currentColor;
+                minColorDistance = temp;
+            }
+        }
+
+        return new AbstractMap.SimpleEntry<>(closestColor, colorList.get(closestColor));
     }
 }
