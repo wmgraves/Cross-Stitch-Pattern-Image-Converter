c1d1ced89f324e3795c8f838c2a1f2ac12f63d44
William G <wmgraves@vt.edu>
2020-09-18 18:37:46 -0400

    Fixed Data Format
    Fixed issue with incorrect formatting for floss codes and descriptions

--- src/IO/FlossDataReader.java
+++ src/IO/FlossDataReader.java
@@ -5,17 +5,21 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashMap;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import javax.swing.*;
 
 public class FlossDataReader {
     /**
      * Read floss data from the default data file
      * @return A HashMap with <Color, <Floss Code, Description, In Stock>>
      */
-    public static HashMap<Color, Triplet<String, String, Boolean>> readFlossDataFile() {
+    public static HashMap<Color, Triplet<String, String, Boolean>> readFlossDataFile(ProgressMonitor monitor) {
         // Open the data file
         File file = new File("src/IO/DMCFlossData.xlsx");
         XSSFWorkbook workbook;
@@ -27,10 +31,11 @@ public class FlossDataReader {
         }
 
         // Create a HashMap and fill it with all of the floss data
+        DataFormatter dataFormatter = new DataFormatter();
         HashMap<Color, Triplet<String, String, Boolean>> returnMap = new HashMap<>();
         XSSFSheet sheet = workbook.getSheet("Data");
 
-        for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+        for (int i = sheet.getFirstRowNum() + 1; i <= sheet.getLastRowNum(); i++) {
             // Get the color
             XSSFRow row = sheet.getRow(i);
             XSSFCell cell = row.getCell(2);
@@ -40,19 +45,21 @@ public class FlossDataReader {
             Color color = new Color(rgb[0], rgb[1], rgb[2]);
 
             // Get related information
-            cell = row.getCell(0);
-            String code = cell.getRawValue();
-
-            cell = row.getCell(1);
-            String description = cell.getRawValue();
-
-            cell = row.getCell(3);
-            boolean inStock = cell.getRawValue().trim().toLowerCase().charAt(0) == 'y';
+            String code = dataFormatter.formatCellValue(row.getCell(0));
+            String description = dataFormatter.formatCellValue(row.getCell(1));
+            Boolean inStock = dataFormatter.formatCellValue(row.getCell(3)).trim().toLowerCase().charAt(0)
+                    == 'y';
 
             // Store row information
             returnMap.put(color, new Triplet(code, description, inStock));
+
+            // Update progress
+            int progress = i * monitor.getMaximum() / sheet.getLastRowNum();
+            monitor.setNote(String.format("Completed %d%%\n", progress));
+            monitor.setProgress(progress);
         }
 
+        monitor.close();
         return returnMap;
     }
 }
